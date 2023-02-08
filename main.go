fb94005fa5df44e0d519c8a5cbb04aa754a53fe9
Rainer Gevers <rainergeve@gmail.com>
2023-02-08 21:57:04 +0200

    init

---
+++ main.go
@@ -0,0 +1,51 @@
+//package Cookiebot
+//
+//import (
+//	"fmt"
+//	"os"
+//)
+
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot <token>")
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register messageCreate as a callback for the messageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Open the websocket and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	<-make(chan struct{})
+	return
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+}
